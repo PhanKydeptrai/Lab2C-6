34a6b0f2a6f247109bbe56dfda95ba35a3d50a80
PhanKydeptrai <kyp194490@gmail.com>
2024-07-04 18:24:33 +0700

    Finish

--- BlazorApp_Test/obj/Debug/net8.0/BlazorApp_Test.AssemblyInfo.cs
+++ BlazorApp_Test/obj/Debug/net8.0/BlazorApp_Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp_Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c20f2835e4f693e9c4fac81011fe4ae79852cab")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp_Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp_Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
